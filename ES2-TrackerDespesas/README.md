328989042b61d8fd7061a1c46eb56931f15dee3f
gianluccasagio <gsaggio2001@gmail.com>
2025-12-08 08:58:19 -0300

    fix: resolve nested repository issue and add project files

---
+++ ES2-TrackerDespesas/README.md
@@ -0,0 +1,2 @@
+# ES2-TrackerDespesas
+Disciplina de Engenharia de Software 2
---
+++ ES2-TrackerDespesas/frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Meu App de Controle de Despesas</h1>
+    </div>
+  )
+}
+
+export default App
